56e11213275487b97377aa7908e8f377214e5745
f2432 <109317080+f2432@users.noreply.github.com>
2023-02-23 14:32:18 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,9 @@
 echo "A executar os testes ..."
 echo
 
-saida=$(echo -e "António Silva\n12\n5\n2010\n" | ./a.out)
-esperado="+-------------------+
-| António Silva |
-+-------------------+
-| faltam 88 dias |
-| para o aniversário |
-+-------------------+"
+saida=$(echo -e "2\n5\n3\n4\n1\n6\n3\n" | ./a.out)
+esperado="O preço total da compra é: 29.00 Euros"
+
 if [ "$saida" != "$esperado" ]; then
     echo "Teste falhou: a saída do programa é diferente do esperado"
     echo "Saída esperada:"
